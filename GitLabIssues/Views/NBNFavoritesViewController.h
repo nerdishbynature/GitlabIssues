55c4fb7cfa58261d7f0ec374b14e202786c21b82
Piet <piet@nerdishbynature.com>
2012-12-16 19:45:31 +0100

    added FavoritesViewController

---
+++ GitLabIssues/Views/NBNFavoritesViewController.h
@@ -0,0 +1,13 @@
+//
+//  NBNFavoritesViewController.h
+//  GitLabIssues
+//
+//  Created by Piet Brauer on 16.12.12.
+//  Copyright (c) 2012 nerdishbynature. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NBNFavoritesViewController : UITableViewController
+
+@end
