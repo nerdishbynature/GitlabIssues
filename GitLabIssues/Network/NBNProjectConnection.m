fa46a65617ba1715687d1014b70488e4c1fc6a40
Piet <piet@nerdishbynature.com>
2012-12-16 20:45:16 +0100

    added project connection added projects viewcontroller

---
+++ GitLabIssues/Network/NBNProjectConnection.m
@@ -0,0 +1,13 @@
+//
+//  NBNProjectConnection.m
+//  GitLabIssues
+//
+//  Created by Piet Brauer on 16.12.12.
+//  Copyright (c) 2012 nerdishbynature. All rights reserved.
+//
+
+#import "NBNProjectConnection.h"
+
+@implementation NBNProjectConnection
+
+@end
