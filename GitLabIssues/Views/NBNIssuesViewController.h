ce46f9b3eed662f1225ed034037e10a91281f655
Piet <piet@nerdishbynature.com>
2012-12-16 21:21:34 +0100

    added init method added issue fetching

--- GitLabIssues/Views/NBNIssuesViewController.h
+++ GitLabIssues/Views/NBNIssuesViewController.h
@@ -7,7 +7,10 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "Project.h"
 
 @interface NBNIssuesViewController : UITableViewController
+
++(NBNIssuesViewController *)loadWithProject:(Project *)project;
 
 @end
--- GitLabIssues/Views/NBNIssuesViewController.m
+++ GitLabIssues/Views/NBNIssuesViewController.m
@@ -7,31 +7,36 @@
 //
 
 #import "NBNIssuesViewController.h"
+#import "Issue.h"
+#import "Author.h"
+#import "NBNIssuesConnection.h"
 
 @interface NBNIssuesViewController ()
+
+@property (nonatomic, retain) Project *project;
+@property (nonatomic, retain) NSArray *issues;
 
 @end
 
 @implementation NBNIssuesViewController
+@synthesize project;
+@synthesize issues;
 
-- (id)initWithStyle:(UITableViewStyle)style
-{
-    self = [super initWithStyle:style];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
++(NBNIssuesViewController *)loadWithProject:(Project *)_project{
+    NBNIssuesViewController *issueViewController = [[NBNIssuesViewController alloc] initWithStyle:UITableViewStylePlain];
+    issueViewController.project = _project;
+    
+    return issueViewController;
 }
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
- 
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    [NBNIssuesConnection loadIssuesForProject:self.project onSuccess:^{
+        self.issues = [Issue findAllSortedBy:@"identifier" ascending:YES];
+        [self.tableView reloadData];
+    }];
 }
 
 - (void)didReceiveMemoryWarning
@@ -44,66 +49,32 @@
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
-#warning Potentially incomplete method implementation.
     // Return the number of sections.
-    return 0;
+    return 1;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-#warning Incomplete method implementation.
     // Return the number of rows in the section.
-    return 0;
+    return self.issues.count;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     static NSString *CellIdentifier = @"Cell";
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
     
-    // Configure the cell...
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
+        cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+    }
+    
+    Issue *issue = [self.issues objectAtIndex:indexPath.row];
+    cell.textLabel.text = issue.title;
+    cell.detailTextLabel.text = [NSString stringWithFormat:@"Created by %@", issue.author.name];
     
     return cell;
 }
-
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-}
-*/
-
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    }   
-    else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
-{
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
 
 #pragma mark - Table view delegate
 
@@ -117,6 +88,16 @@
      [self.navigationController pushViewController:detailViewController animated:YES];
      [detailViewController release];
      */
+}
+
+- (void)dealloc
+{
+    self.project = nil;
+    self.issues = nil;
+    
+    [project release];
+    [issues release];
+    [super dealloc];
 }
 
 @end
