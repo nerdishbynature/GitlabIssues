0d07a70c077b9ed0b3b0ce1cf8c69d3a8712da7e
Piet <piet@nerdishbynature.com>
2012-12-17 10:05:05 +0100

    added navigation to IssuesViewController

--- GitLabIssues/Views/NBNFavoritesViewController.m
+++ GitLabIssues/Views/NBNFavoritesViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "NBNFavoritesViewController.h"
+#import "NBNIssuesViewController.h"
 #import "Project.h"
 
 @interface NBNFavoritesViewController ()
@@ -101,15 +102,12 @@
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    // Navigation logic may go here. Create and push another view controller.
-    /*
-     <#DetailViewController#> *detailViewController = [[<#DetailViewController#> alloc] initWithNibName:@"<#Nib name#>" bundle:nil];
-     // ...
-     // Pass the selected object to the new view controller.
-     [self.navigationController pushViewController:detailViewController animated:YES];
-     [detailViewController release];
-     */
+    NBNIssuesViewController *issuesViewController = [NBNIssuesViewController loadWithProject:(Project *)[self.favoriteArray objectAtIndex:indexPath.row]];
+    [self.navigationController pushViewController:issuesViewController animated:YES];
+    
+    [issuesViewController release];
 }
+
 -(void)dealloc{
     self.favoriteArray = nil;
     
