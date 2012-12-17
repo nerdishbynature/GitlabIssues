49eed716bc0fb3399a42cefdc8b8d4f5445b9a39
Piet <piet@nerdishbynature.com>
2012-12-17 10:08:37 +0100

    added issue filter view controller

---
+++ GitLabIssues/Views/NBNIssueFilterViewController.h
@@ -0,0 +1,13 @@
+//
+//  NBNIssueFilterViewController.h
+//  GitLabIssues
+//
+//  Created by Piet Brauer on 17.12.12.
+//  Copyright (c) 2012 nerdishbynature. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NBNIssueFilterViewController : UIViewController
+
+@end
