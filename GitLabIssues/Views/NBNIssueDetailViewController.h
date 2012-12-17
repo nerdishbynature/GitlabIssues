c57022bbc0f6eb712a71de190a5a228622c364f5
Piet <piet@nerdishbynature.com>
2012-12-17 09:57:31 +0100

    added issue detail view controller added NSHash categories for simply creating MD5 Hash needed for gravatar images

---
+++ GitLabIssues/Views/NBNIssueDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  NBNIssueDetailViewController.h
+//  GitLabIssues
+//
+//  Created by Piet Brauer on 17.12.12.
+//  Copyright (c) 2012 nerdishbynature. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NBNIssueDetailViewController : UIViewController
+
+@end
---
+++ GitLabIssues/Views/NBNIssueDetailViewController.m
@@ -0,0 +1,38 @@
+//
+//  NBNIssueDetailViewController.m
+//  GitLabIssues
+//
+//  Created by Piet Brauer on 17.12.12.
+//  Copyright (c) 2012 nerdishbynature. All rights reserved.
+//
+
+#import "NBNIssueDetailViewController.h"
+
+@interface NBNIssueDetailViewController ()
+
+@end
+
+@implementation NBNIssueDetailViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
