b2fedbb62a89e195fc5d221586a527099bcfe796
Piet <piet@nerdishbynature.com>
2012-12-18 09:59:04 +0100

    created issue edit view controller

---
+++ GitLabIssues/Views/NBNIssueEditViewController.h
@@ -0,0 +1,13 @@
+//
+//  NBNIssueEditViewController.h
+//  GitLabIssues
+//
+//  Created by Piet Brauer on 18.12.12.
+//  Copyright (c) 2012 nerdishbynature. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NBNIssueEditViewController : UITableViewController
+
+@end
