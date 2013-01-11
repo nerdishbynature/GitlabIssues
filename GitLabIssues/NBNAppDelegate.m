9e58193033a44062ea6f710ba20576657d8d0ad9
Piet <piet@nerdishbynature.com>
2013-01-11 08:44:54 +0100

    database is saved on entering background on entering foreground sessions are deleted - prevents old session and server refusing

--- GitLabIssues/NBNAppDelegate.m
+++ GitLabIssues/NBNAppDelegate.m
@@ -63,6 +63,7 @@
 {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    [[NSManagedObjectContext MR_contextForCurrentThread] saveNestedContexts];
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application
@@ -74,6 +75,14 @@
 - (void)applicationWillEnterForeground:(UIApplication *)application
 {
     // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+    NSArray *sessionArray = [Session findAll];
+    
+    for (Session *session in sessionArray) {
+        PBLog(@"deleting %@", session.private_token);
+        [[NSManagedObjectContext MR_contextForCurrentThread] deleteObject:session];
+    }
+    
+    PBLog(@"sessions %@", [Session findAll]);
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application
