07624e514a28320dde3b6ba4433d2cc5cea12a32
Piet Brauer <piet@nerdishbynature.com>
2013-01-22 22:04:22 +0100

    added bar button touching added textfield entering with keyboard (required for using FormKit) added bubble item recognition

--- Frank/features/step_definitions/pb_step_additions.rb
+++ Frank/features/step_definitions/pb_step_additions.rb
@@ -10,3 +10,20 @@ When /^I touch "(.*?)" picker button$/ do |mark|
   quote = get_selector_quote(mark)
   touch("view:'UIToolbarTextButton' marked:#{quote}#{mark}#{quote}")
 end
+
+When /^I touch the "(.*?)" bar button$/ do |mark|
+	wait_for_nothing_to_be_animating 
+	quote = get_selector_quote(mark)
+	touch("view:'UIBarButtonItem' marked:#{quote}#{mark}#{quote}")
+end
+
+When /^I fill in text fields as follows using the keyboard :$/ do |table|
+  table.hashes.each do |row|
+    step %Q|I enter the text "#{row['text']}" from keyboard to the textfield "#{row['field']}"|
+  end
+end
+
+Then /^I should see a bubble item marked "(.*?)"$/ do |mark|
+  quote = get_selector_quote(mark)
+  touch("view:'HEBubbleViewItem' marked:#{quote}#{mark}#{quote}")
+end
