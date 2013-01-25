5705b0bfc02b7fea78d17dc766405c8617c8848e
Piet Brauer <piet@nerdishbynature.com>
2013-01-25 19:35:58 +0100

    error parsing is now done in issue creation, added flurry

--- GitLabIssues/Models/Issue.m
+++ GitLabIssues/Models/Issue.m
@@ -130,7 +130,12 @@
         [request setCompletionBlock:^{
             NSDictionary *responseDict = [NSJSONSerialization JSONObjectWithData:request.responseData options:kNilOptions error:nil];
             
-            [self parseServerResponse:responseDict];
+            if (request.responseStatusCode == 201) {
+                [self parseServerResponse:responseDict];
+            } else{
+                UIAlertView *alert = [[[UIAlertView alloc] initWithTitle:@"" message:@"An error occured, check if you have the rights to perform this action." delegate:nil cancelButtonTitle:@"Dismiss" otherButtonTitles:nil] autorelease];
+                [alert show];
+            }
             block();
         }];
         
