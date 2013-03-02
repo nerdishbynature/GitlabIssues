e1de14538f06e526bc7329234f04b2f0c39558c9
Piet Brauer <piet@nerdishbynature.com>
2013-03-02 16:55:57 +0100

    rearranged files

--- GitLabIssues/Vendor/MKNetworkKit/MKNetworkKit.h
+++ GitLabIssues/Vendor/MKNetworkKit/MKNetworkKit.h
@@ -65,7 +65,7 @@
 #import "Categories/NSAlert+MKNetworkKitAdditions.h"
 #endif
 
-#import "Reachability/Reachability.h"
+#import "Reachability.h"
 
 #import "MKNetworkOperation.h"
 #import "MKNetworkEngine.h"
